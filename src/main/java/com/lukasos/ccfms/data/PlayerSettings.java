fe7d997e31a40ff75dde0fa0144ea8c583f3c439
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-17 08:07:10 +0300

    Delete src/main/java/com/lukasos/ccfms/data/PlayerSettings.java

--- src/main/java/com/lukasos/ccfms/data/PlayerSettings.java
+++
@@ -1,7 +0,0 @@
-package com.lukasos.ccfms.data;
-
-public class PlayerSettings {
-    public boolean acceptTpaRequests = true;
-    public boolean teleportSound = true;
-    public boolean backOnDeath = true;
-}
