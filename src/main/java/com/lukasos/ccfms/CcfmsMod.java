44f68f7c888dc44220fdb446c5cb9473d183b095
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-17 08:05:06 +0300

    Remove SettingsManager and update teleport method

--- src/main/java/com/lukasos/ccfms/CcfmsMod.java
+++ src/main/java/com/lukasos/ccfms/CcfmsMod.java
@@ -3,12 +3,10 @@ package com.lukasos.ccfms;
 import com.lukasos.ccfms.commands.HomeCommands;
 import com.lukasos.ccfms.commands.MiscCommands;
 import com.lukasos.ccfms.commands.RtpCommand;
-import com.lukasos.ccfms.commands.SettingsCommand;
 import com.lukasos.ccfms.commands.TpaCommands;
 import com.lukasos.ccfms.data.BackManager;
 import com.lukasos.ccfms.data.HomeLocation;
 import com.lukasos.ccfms.data.HomeManager;
-import com.lukasos.ccfms.data.SettingsManager;
 import com.lukasos.ccfms.data.TpaManager;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -16,7 +14,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.RelativeMovement;
+import net.minecraft.world.entity.Relative;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.HashMap;
@@ -27,7 +25,6 @@ public class CcfmsMod implements ModInitializer {
     public static final String MOD_ID = "ccfms";
 
     public static HomeManager homeManager;
-    public static SettingsManager settingsManager;
     public static final TpaManager tpaManager = new TpaManager();
     public static final BackManager backManager = new BackManager();
     public static final Map<String, HomeLocation> spawnPoints = new HashMap<>();
@@ -36,30 +33,24 @@ public class CcfmsMod implements ModInitializer {
     public void onInitialize() {
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
             homeManager = new HomeManager(server);
-            settingsManager = new SettingsManager(server);
-        });
-
-        ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
-            if (settingsManager != null) settingsManager.save();
         });
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             HomeCommands.register(dispatcher);
             RtpCommand.register(dispatcher);
             TpaCommands.register(dispatcher);
-            SettingsCommand.register(dispatcher);
             MiscCommands.register(dispatcher);
         });
     }
 
     public static void teleport(ServerPlayer player, ServerLevel world, double x, double y, double z, float yaw, float pitch) {
-        player.teleportTo(world, x, y, z, Set.<RelativeMovement>of(), yaw, pitch);
+        player.teleportTo(world, x, y, z, Set.<Relative>of(), yaw, pitch, false);
     }
 
     public static HomeLocation currentLocation(ServerPlayer player) {
         Vec3 pos = player.position();
         return new HomeLocation(
-                player.level().dimension().location().toString(),
+                player.level().dimension().identifier().toString(),
                 pos.x, pos.y, pos.z,
                 player.getYRot(), player.getXRot()
         );
@@ -67,7 +58,7 @@ public class CcfmsMod implements ModInitializer {
 
     public static ServerLevel worldFromDimensionId(MinecraftServer server, String dimensionId) {
         for (ServerLevel w : server.getAllLevels()) {
-            if (w.dimension().location().toString().equals(dimensionId)) {
+            if (w.dimension().identifier().toString().equals(dimensionId)) {
                 return w;
             }
         }
