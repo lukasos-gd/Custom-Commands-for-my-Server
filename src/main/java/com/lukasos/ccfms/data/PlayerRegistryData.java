211ca0e5482ae939ab3932075e7a82e69da8d5b5
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-21 15:37:35 +0300

    Add PlayerRegistryData class for player records

---
+++ src/main/java/com/lukasos/ccfms/data/PlayerRegistryData.java
@@ -0,0 +1,8 @@
+package com.lukasos.ccfms.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerRegistryData {
+    public Map<String, PlayerRecord> players = new HashMap<>();
+}
