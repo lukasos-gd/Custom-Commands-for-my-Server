99efb70d832ac9fb8121f2f7baa7d07f277a04cd
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-21 15:31:23 +0300

    Add BanHistoryData class for storing ban records

---
+++ src/main/java/com/lukasos/ccfms/data/BanHistoryData.java
@@ -0,0 +1,9 @@
+package com.lukasos.ccfms.data;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BanHistoryData {
+    public Map<String, List<BanRecord>> history = new HashMap<>();
+}
