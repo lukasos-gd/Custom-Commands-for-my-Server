35a9a625f280d72d69f001f739b8f3e509689333
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-21 08:53:47 +0300

    Update TpaManager.java

--- src/main/java/com/lukasos/ccfms/data/TpaManager.java
+++ src/main/java/com/lukasos/ccfms/data/TpaManager.java
@@ -7,20 +7,24 @@ import java.util.UUID;
 public class TpaManager {
     public static final long EXPIRY_MS = 60_000;
 
+    public enum RequestType { NORMAL, HERE }
+
     public static class Request {
         public final UUID from;
         public final long timestamp;
+        public final RequestType type;
 
-        public Request(UUID from, long timestamp) {
+        public Request(UUID from, long timestamp, RequestType type) {
             this.from = from;
             this.timestamp = timestamp;
+            this.type = type;
         }
     }
 
     private final Map<UUID, Request> pending = new HashMap<>();
 
-    public void addRequest(UUID from, UUID to) {
-        pending.put(to, new Request(from, System.currentTimeMillis()));
+    public void addRequest(UUID from, UUID to, RequestType type) {
+        pending.put(to, new Request(from, System.currentTimeMillis(), type));
     }
 
     public Request getRequest(UUID to) {
