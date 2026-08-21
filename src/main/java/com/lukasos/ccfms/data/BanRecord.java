96655f2a284e9357d870dec8959ceabdb7545664
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-21 08:54:36 +0300

    Add BanRecord class for managing ban details

---
+++ src/main/java/com/lukasos/ccfms/data/BanRecord.java
@@ -0,0 +1,9 @@
+package com.lukasos.ccfms.data;
+
+public class BanRecord {
+    public String targetName;
+    public String reason;
+    public Long expiresAt;
+    public String bannedBy;
+    public long timestamp;
+}
