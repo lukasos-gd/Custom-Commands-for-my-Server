13a2674fb4712f8dbcf5fb0c65c44e02a0105165
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-31 13:39:45 +0300

    Add description field to ShopCategory class

--- src/main/java/com/lukasos/ccfms/menu/ShopCategory.java
+++ src/main/java/com/lukasos/ccfms/menu/ShopCategory.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 public class ShopCategory {
     public final String name;
+    public final String description;
     public final Item icon;
     public final List<ShopEntry> entries;
 
-    public ShopCategory(String name, Item icon, List<ShopEntry> entries) {
+    public ShopCategory(String name, String description, Item icon, List<ShopEntry> entries) {
         this.name = name;
+        this.description = description;
         this.icon = icon;
         this.entries = entries;
     }
